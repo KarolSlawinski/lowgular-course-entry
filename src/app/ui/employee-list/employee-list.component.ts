f73c4b87e98ec6c9243da860b301c8ff69ccc8c1
KarolSlawinski <karol.slawinski1@gmail.com>
2022-11-15 13:21:10 +0100

    feat: addded faces

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -5,13 +5,17 @@ import { EmployeeModel } from '../../model/employee.model';
 
 @Component({
   selector: 'employee-list',
-  templateUrl: './employee-list.component.html',
+ templateUrl: './employee-list.component.html',
+  //template: '<h1>Inaczej</h1>',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
   constructor(private _httpClient: HttpClient) {}
+  // title: string = 'Tekst który sie pojawi w HTML';
+  //napis: string = 'Cos innego niz title :)';
   data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
+  //data = [{name: 'Jacek'}, {name: 'Wojtek'}];
 
 
 }
